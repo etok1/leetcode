c549940553203c45f38f1a8805a420c9e7847140
Eva Tokmakova <eva.tokmakova12@gmail.com>
2024-01-24 21:10:50 +0600

    added changes in 5 file

--- 5.js
+++ 5.js
@@ -8,15 +8,6 @@
  */
 var isPalindrome = function isPalindrome(x) {
   let xString = x.toString();
-  let xArr = xString.split("");
-  let lastEl = xArr[xArr.length - 1];
-  for (let i = 0; i < xArr.length; i++) {
-    // console.log("working");
-    // console.log(lastEl);
-    if (xArr[0] === lastEl) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  let xReversed = xString.split("").reverse().join("");
+  return xString === xReversed;
 };

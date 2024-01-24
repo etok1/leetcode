6b851398b40d1581ce1a4fde061af022a912b8c3
Eva Tokmakova <eva.tokmakova12@gmail.com>
2024-01-24 21:20:14 +0600

    added changes in 4 file

--- 4.js
+++ 4.js
@@ -18,9 +18,11 @@
 var twoSum = function (nums, target) {
   let arr = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] + nums[i - 1] === target) {
-      arr.push(i - 1);
-      arr.push(i);
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        arr.push(i);
+        arr.push(j);
+      }
     }
   }
   return arr;
